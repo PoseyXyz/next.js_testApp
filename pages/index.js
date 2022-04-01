1433f067a80e3a352d40ef4be29251d423104f02
Posey <adeposi60@gmail.com>
2022-04-01 22:33:35 +0100

    Testing data from sanity

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({data}) {
+  const recipes = data.recipes
   return (
     <div className={styles.container}>
       <Head>
@@ -14,6 +15,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <h1>{recipes[0].title}</h1>
         </h1>
 
         <p className={styles.description}>
@@ -66,4 +68,13 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+export function getStaticProps(){
+  return {
+    props:{
+      data:{
+        recipes:[{title:'Pineapple Smoothie'}]
+      }
+    }
+  }
 }
